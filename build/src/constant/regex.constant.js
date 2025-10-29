ed4a46baa52b326fed861d3efe7cb86da73c21c6
Sagar Ninave <157126857+sagar-ninave-amla@users.noreply.github.com>
2025-10-29 23:25:02 +0530

    remove build folder

--- build/src/constant/regex.constant.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const regExp = {
-    emailRegExp: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
-    passwordRegExp: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-};
-exports.default = regExp;
-//# sourceMappingURL=regex.constant.js.map
